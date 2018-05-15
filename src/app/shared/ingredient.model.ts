e451a10a48e0e19c602a7b000b4007a0c8fa7115
verlil <vernigoralilia@gmail.com>
2018-05-15 19:34:46 +0300

    ingredient.model created

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,6 @@
+export class Ingredient {
+  constructor(public name: string, public amount: number) {
+    //this will create variables 'name' and 'ammount' and assign them values
+    //that you send using a constructor
+  }
+}
