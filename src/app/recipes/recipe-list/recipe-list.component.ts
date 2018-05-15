fcc8819c9b3930f5d764c54067f1d752db1d3317
verlil <vernigoralilia@gmail.com>
2018-05-15 19:26:13 +0300

    recipe.model added, basic recipe markup

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,8 +8,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
-  constructor() { }
+  recipes: Recipe[] = [
+    new Recipe('Fried Eggs', 'So tasty!', 'https://i0.wp.com/eatlikeamanda.com/wp-content/uploads/2017/04/img_7995.jpg'),
+    new Recipe('Fries', 'So tasty!!!', 'http://images.bigoven.com/image/upload/t_recipe-256/all-star-french-fries-dd49eb.jpg')
+  ]
 
   ngOnInit() {
   }
