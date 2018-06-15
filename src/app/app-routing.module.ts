25ab157ee78e49c58c3264d30ad1baec5d195bfe
verlil <vernigoralilia@gmail.com>
2018-06-16 02:34:21 +0300

    basic routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import {RouterModule, Routes} from '@angular/router';
+import {RecipesComponent} from './recipes/recipes.component';
+import {ShoppingListComponent} from './shopping-list/shopping-list.component';
+import {NgModule} from '@angular/core';
+
+const appRoutes: Routes = [
+  {path: '', redirectTo: '/recipes', pathMatch: 'full'},
+  {path: 'recipes', component: RecipesComponent},
+  {path: 'shopping-list', component: ShoppingListComponent}
+];
+
+// @NgModule transforms an ordinary TS class into a module
+@NgModule({
+  imports: [
+    RouterModule.forRoot(appRoutes)
+  ],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule {
+
+}
