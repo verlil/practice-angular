51d1b9805e545dedfe4b9e5a8a1d91771d88969c
verlil <vernigoralilia@gmail.com>
2018-07-20 06:05:26 +0300

    moving services to the core.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,8 @@ import {NgModule} from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import {AppComponent} from './app.component';
-import {ShoppingListService} from './shopping-list/shopping-list.service';
 import {AppRoutingModule} from './app-routing.module';
-import {RecipeService} from './recipes/recipe.service';
-import {DataStorageService} from './shared/data-storage.service';
 import {HttpClientModule} from '@angular/common/http';
-import {AuthService} from './auth/auth.service';
-import {AuthGuardService} from './auth/auth-guard.service';
 import {SharedModule} from './shared/shared.module';
 import {CoreModule} from './core/core.module';
 
@@ -25,7 +20,7 @@ import {CoreModule} from './core/core.module';
     SharedModule,
     CoreModule
   ],
-  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuardService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,6 +3,11 @@ import {HeaderComponent} from './header/header.component';
 import {HomeComponent} from './home/home.component';
 import {SharedModule} from '../shared/shared.module';
 import {AppRoutingModule} from '../app-routing.module';
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
+import {RecipeService} from '../recipes/recipe.service';
+import {DataStorageService} from '../shared/data-storage.service';
+import {AuthService} from '../auth/auth.service';
+import {AuthGuardService} from '../auth/auth-guard.service';
 
 @NgModule({
   declarations: [
@@ -16,6 +21,13 @@ import {AppRoutingModule} from '../app-routing.module';
   exports: [
     AppRoutingModule,
     HeaderComponent // exporting this declaration you're able to use <app-header> selector in app.component
+  ],
+  providers: [
+    ShoppingListService,
+    RecipeService,
+    DataStorageService,
+    AuthService,
+    AuthGuardService
   ]
 })
 export class CoreModule {
